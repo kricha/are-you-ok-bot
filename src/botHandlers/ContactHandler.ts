b9767e5f2f02146722bf6a78d7ed13cfd771c5a8
Aleksei <krich.al.vl@gmail.com>
2022-03-22 20:26:04 +0200

    feat: add process contact from attachment

--- src/botHandlers/ContactHandler.ts
+++ src/botHandlers/ContactHandler.ts
@@ -2,62 +2,74 @@ import {db} from "../db";
 import {BotManagerInterface} from "../BotManager";
 import {parsePhoneNumber} from "libphonenumber-js";
 import {hash} from "../utils";
+import {RESTRICTED_COUNTRIES} from "../constants";
 
 export default function ContactHandler(BotManager: BotManagerInterface) {
     BotManager.bot.on("contact", (msg) => {
-        db.getUserById(msg.contact.user_id)
+        const currentUid = msg.from.id;
+        const ownContact = msg.contact.user_id === msg.from.id;
+        db.getUserById(currentUid)
             .then(
                 res => {
-                    const phone = msg.contact.phone_number;
+                    const phone = msg.contact.phone_number.replace('+', '');
                     const phoneNumber = parsePhoneNumber(`+${phone}`);
-                    const active = phoneNumber.country !== 'RU';
+                    const isRestrictedContact = RESTRICTED_COUNTRIES.includes(phoneNumber.country);
                     const phoneHash = hash(msg.contact.phone_number);
-                    db.updateUserContactInformation(msg.contact.user_id, active, phoneHash, phoneNumber.country)
-                        .then(result => {
-                                if (phoneNumber.country === 'RU') {
-                                    BotManager.sendRussianWarshipGoToHellReply(msg.chat.id, msg.message_id);
-                                } else {
-                                    BotManager.sendThanksPhoneShareReply(msg.chat.id, res.lang, phoneHash);
-                                    db.getAllWaitSubsByPhoneHash(phoneHash)
-                                        .then(res => {
-                                            if (!res.length) {
-                                                return;
-                                            }
-                                            for (let row of res) {
-                                                db.addOrUpdateUserSubs(row.uid, msg.contact.user_id, row.alias)
-                                                    .then(() => {
-                                                        db.deleteWaitUserPhoneSubById(row.id);
-                                                    })
-                                                ;
-                                            }
-                                        })
-                                    ;
-                                    if (msg.from.username !== undefined) {
-                                        db.getAllWaitSubsByUsername(msg.from.username.toLowerCase())
+                    if (!ownContact) {
+                        const aliasArray = [];
+                        if (msg.contact.first_name) {
+                            aliasArray.push(msg.contact.first_name);
+                        }
+                        if (msg.contact.last_name) {
+                            aliasArray.push(msg.contact.last_name);
+                        }
+                        let alias = aliasArray.length ? aliasArray.join(' ') : '';
+                        BotManager.processSharedPhoneContact(currentUid, msg.message_id, phoneNumber, res.lang, alias);
+                    } else {
+                        db.updateUserContactInformation(currentUid, !isRestrictedContact, phoneHash, phoneNumber.country)
+                            .then(result => {
+                                    if (phoneNumber.country === 'RU') {
+                                        BotManager.sendRussianWarshipGoToHellReply(msg.chat.id, msg.message_id);
+                                    } else {
+                                        BotManager.sendThanksPhoneShareReply(msg.chat.id, res.lang, phoneHash);
+                                        db.getAllWaitSubsByPhoneHash(phoneHash)
                                             .then(res => {
                                                 if (!res.length) {
                                                     return;
                                                 }
                                                 for (let row of res) {
-                                                    db.addOrUpdateUserSubs(row.uid, msg.contact.user_id, row.alias)
+                                                    db.addOrUpdateUserSubs(row.uid, currentUid, row.alias)
                                                         .then(() => {
-                                                            db.deleteWaitUserUsernameSubById(row.id);
+                                                            db.deleteWaitUserPhoneSubById(row.id);
                                                         })
                                                     ;
                                                 }
                                             })
                                         ;
+                                        if (msg.from.username !== undefined) {
+                                            db.getAllWaitSubsByUsername(msg.from.username.toLowerCase())
+                                                .then(res => {
+                                                    if (!res.length) {
+                                                        return;
+                                                    }
+                                                    for (let row of res) {
+                                                        db.addOrUpdateUserSubs(row.uid, currentUid, row.alias)
+                                                            .then(() => {
+                                                                db.deleteWaitUserUsernameSubById(row.id);
+                                                            })
+                                                        ;
+                                                    }
+                                                })
+                                            ;
+                                        }
                                     }
+                                    BotManager.sendHelpMessage(msg.chat.id, res.lang, res.tz, true);
                                 }
-                                setTimeout(()=>{
-                                    BotManager.requestTzUpdate(msg.chat.id, res.lang);
-                                }, 1000);
-                                setTimeout(()=>{
-                                    BotManager.sendHelpMessage(msg.chat.id, res.lang);
-                                }, 10000);
-                            }
-                        )
-                        .catch(reason => console.log('updateUserContactInformation error', reason));
-                });
+                            )
+                            .catch(reason => console.log('updateUserContactInformation error', reason))
+                        ;
+                    }
+                })
+        ;
     });
 }
