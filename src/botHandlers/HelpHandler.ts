d4003bfca91babd7f42866e794ff1136412daec3
Aleksei <krich.al.vl@gmail.com>
2022-03-22 20:26:28 +0200

    fix: split help into help and command_help

--- src/botHandlers/HelpHandler.ts
+++ src/botHandlers/HelpHandler.ts
@@ -1,8 +1,5 @@
 import {db} from "../db";
-import {parsePhoneNumber} from "libphonenumber-js";
-import {hash} from "../utils";
-import botManager, {BotManagerInterface} from "../BotManager";
-import i18n from "../i18n";
+import {BotManagerInterface} from "../BotManager";
 
 export default function HelpHandler(BotManager: BotManagerInterface) {
     BotManager.bot.onText(/^\/help/, (msg, match) => {
@@ -10,7 +7,21 @@ export default function HelpHandler(BotManager: BotManagerInterface) {
             .then(res => {
                 const lng = res.lang;
                 if (res.active === 1) {
-                    BotManager.sendHelpMessage(msg.chat.id, lng)
+                    BotManager.sendHelpMessage(msg.chat.id, lng, res.tz)
+                } else {
+                    BotManager.sendNotActiveReply(msg.chat.id, msg.message_id, lng)
+                    BotManager.requestPhone(msg.chat.id, lng);
+                }
+            })
+        ;
+    });
+
+    BotManager.bot.onText(/^\/command_help/, (msg, match) => {
+        db.getUserById(msg.from.id)
+            .then(res => {
+                const lng = res.lang;
+                if (res.active === 1) {
+                    BotManager.sendCommandHelpMessage(msg.chat.id, lng)
                 } else {
                     BotManager.sendNotActiveReply(msg.chat.id, msg.message_id, lng)
                     BotManager.requestPhone(msg.chat.id, lng);
