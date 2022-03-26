f5b85e60f09133438b6d96340ad2ec58142d45ab
Aleksei <krich.al.vl@gmail.com>
2022-03-26 21:43:57 +0200

    fix: udpate constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,5 @@
 export const LANG_UA = 'lang_ua';
 export const LANG_EN = 'lang_en';
-export const REPORT_HOURS = '21';
+export const REPORT_HOURS = '22';
 export const AYOK_DATE_FORMAT = 'YYYYMMDD';
 export const RESTRICTED_COUNTRIES = ['RU'];
-export const NOT_ASWER_MINS_ALERT = 29;
