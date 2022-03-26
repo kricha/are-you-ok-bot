9243a93d98f536c7faa65612bc7efbeb017adb0c
Aleksei <krich.al.vl@gmail.com>
2022-03-26 19:17:41 +0200

    fix: add token marker to logs in AYOK cron handler

--- src/CronHandlers/AreYouOkCronHandler.ts
+++ src/CronHandlers/AreYouOkCronHandler.ts
@@ -1,5 +1,5 @@
 import cron from 'node-cron';
-import {areYouOkHours, tzMinutes} from '../utils';
+import {areYouOkHours, getUnique, tzMinutes} from '../utils';
 import moment from 'moment';
 import {logger} from '../logger';
 import {db} from '../db';
@@ -7,6 +7,7 @@ import BotManager from '../BotManager';
 
 export const AreYouOkCronHandler = () => {
     cron.schedule('0 * * * *', () => {
+        const token = getUnique();
         const processHours = areYouOkHours;
 
         for (const offset of tzMinutes) {
@@ -20,17 +21,17 @@ export const AreYouOkCronHandler = () => {
             const timezones = processHours[ayokHour];
             if (timezones.length) {
                 const timeZonesString = timezones.map(tz => `GMT${(parseInt(tz) > 0 ? '+' : '') + (parseInt(tz) / 60).toString()}`).join(', ');
-                logger.info(`Start processing AYOK request for ${ayokHour}:00 in ${timeZonesString} timezones`);
+                logger.info(`[${token}] Start processing AYOK request for ${ayokHour}:00 in ${timeZonesString} timezones`);
                 db.getUsersByTz(timezones)
                     .then((result) => {
                         if (result.length) {
                             const usersInTimeZones = result.map(row => `${row.uid} ${row.username ? `(${row.username})` : ''}`).join(', ');
-                            logger.info(`Users ${usersInTimeZones} start getting AYOK ${ayokHour}:00 requests`);
+                            logger.info(`[${token}] Users ${usersInTimeZones} start getting AYOK ${ayokHour}:00 requests`);
                             for (const row of result) {
                                 BotManager.sendAreYouOkRequest(row.uid, row.lang, `ayok_${ayokHour}`)
                                     .then(res => {
                                         const message_id = res.message_id;
-                                        logger.info(`Successful sent request to chat ${res.chat.id} in message ${message_id}`);
+                                        logger.info(`[${token}] Successful sent request to chat ${res.chat.id} in message ${message_id}`);
                                         const date = moment(res.date * 1000).utcOffset(row.tz).add(30, 'm');
                                         db.addToWaitAnswer(res.chat.id, date.unix(), message_id);
                                     })
@@ -38,7 +39,7 @@ export const AreYouOkCronHandler = () => {
                                 ;
                             }
                         } else {
-                            logger.info(`No users for AYOK ${ayokHour}:00 request in timezones ${timeZonesString}`);
+                            logger.info(`[${token}] No users for AYOK ${ayokHour}:00 request in timezones ${timeZonesString}`);
                         }
                     })
                 ;
