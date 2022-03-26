2f428d3e6576c484420bd979d91822241dcb7463
Aleksei <krich.al.vl@gmail.com>
2022-03-26 19:20:33 +0200

    fix: rebuild not answering bot and ayok cron handlers

--- src/BotHandlers/AreYouOkBotHandler.ts
+++ src/BotHandlers/AreYouOkBotHandler.ts
@@ -1,29 +1,81 @@
 import {db} from '../db';
 import moment from 'moment';
-import {AYOK_DATE_FORMAT} from '../constants';
+import {AYOK_DATE_FORMAT, NOT_ASWER_MINS_ALERT} from '../constants';
 import i18n from '../i18n';
 import BotManagerInterface from '../Interfaces/BotManagerInterface';
+import {logger} from '../logger';
+import {getUnique} from '../utils';
 
 export default function AreYouOkBotHandler(BotManager: BotManagerInterface) {
     BotManager.bot.on('callback_query', query => {
         const uid = query.from.id;
         const langArray = query.data.split('_');
-        if (langArray.length === 2 && langArray[0] === 'ayok') {
+        if (langArray.length >= 2 && langArray[0] === 'ayok') {
+            const token = getUnique();
+            logger.info(`[${token}] Got new AYOK request from ${uid}`);
             db.getUserById(uid)
                 .then(res => {
                     const ayokHour = langArray[1];
-                    const messageDT =  moment(query.message.date*1000);
+                    const now = moment();
+                    const messageDT = moment(query.message.date * 1000);
+                    const answerDiffDays = messageDT.diff(now, 'days');
                     const dateKey = messageDT.format(AYOK_DATE_FORMAT);
-                    if (messageDT.diff(moment(), 'days') < 1) {
+                    const messageTimeFromCb = langArray[2] ? moment(parseInt(langArray[2]) * 1000) : false;
+                    logger.info(`[${token}] ${uid} answered after ${now.diff(messageDT, 'minutes')} minutes`);
+                    db.getWaitAnswerByUid(uid)
+                        .then(waitAnswer => {
+                            let msgIdToEdit = query.message.message_id;
+                            if (waitAnswer && waitAnswer.message_id !== msgIdToEdit) {
+                                msgIdToEdit = waitAnswer.message_id;
+                                BotManager.bot.deleteMessage(uid, query.message.message_id.toString())
+                                    .then(()=>{
+                                        logger.info(`[${token}] Deleted current request for ${uid}`);
+                                    })
+                                ;
+                            }
+                            BotManager.bot.editMessageText(i18n.t('good.thanks', {lng: res.lang}), {
+                                chat_id: uid,
+                                message_id: msgIdToEdit
+                            });
+                            db.deleteFromWaitAnswer(uid);
+                        })
+                    ;
+                    if (answerDiffDays < 1) {
+                        /** SENDING NOTIFICATION ABOUT OK ANSWER START **/
+                        if (moment.isMoment(messageTimeFromCb)) {
+                            const minutesFromAlert = now.diff(messageTimeFromCb, 'minutes');
+                            if (minutesFromAlert >= NOT_ASWER_MINS_ALERT) {
+                                logger.info(`[${token}] ${uid} answering answers too late, after ${minutesFromAlert} from original ask`);
+                                db.getAllSubscribersByUid(res.uid)
+                                    .then(subscribers => {
+                                        if (!subscribers.length) {
+                                            return;
+                                        }
+                                        logger.info(`[${token}] Start notifying subscribers of ${uid} about ok state`);
+                                        for (const sub of subscribers) {
+                                            BotManager.bot.sendMessage(
+                                                sub.uid,
+                                                i18n.t('already.answered', {
+                                                    lng: sub.lang,
+                                                    uid: sub.uid,
+                                                    alias: sub.alias,
+                                                }),
+                                                {parse_mode: 'HTML'}
+                                            )
+                                                .then(() => {
+                                                    logger.info(`[${token}] Sent info to ${sub.uid} that ${uid} is ok`);
+                                                })
+                                                .catch(error => {
+                                                    logger.warn(`[${token}] Error sending info to ${sub.uid} that ${uid} is ok`, error.message);
+                                                })
+                                            ;
+                                        }
+                                    })
+                                ;
+                            }
+                        }
                         db.updateAreYouOkField(uid, `'$.${dateKey}.${ayokHour}'`);
-                    } else {
-                        console.log(messageDT, moment());
                     }
-                    BotManager.bot.editMessageText(i18n.t('good.thanks', {lng: res.lang}), {
-                        chat_id: uid,
-                        message_id: query.message.message_id
-                    });
-                    db.deleteFromWaitAnswer(uid);
                 })
             ;
         }
--- src/CronHandlers/NotAnsweringCronHandler.ts
+++ src/CronHandlers/NotAnsweringCronHandler.ts
@@ -3,23 +3,28 @@ import {db} from '../db';
 import moment from 'moment';
 import BotManager from '../BotManager';
 import i18n from '../i18n';
+import {AYOK_DATE_FORMAT, NOT_ASWER_MINS_ALERT} from '../constants';
 
 export const NotAnsweringCronHandler = () => {
-    cron.schedule('*/30 * * * *', () => {
+    cron.schedule('*/10 * * * *', () => {
         db.getAllWaitingAnswers(moment().unix())
             .then(res => {
                 if (res.length) {
                     for (const row of res) {
-                        db.getAllSubscriberByUid(row.uid)
+                        const userTsForMoment = row.ts * 1000;
+                        const minutesDiff = moment()
+                            .diff(
+                                moment(userTsForMoment),
+                                'minute'
+                            )
+                        ;
+                        if (minutesDiff < NOT_ASWER_MINS_ALERT) {
+                            return;
+                        }
+                        db.getAllSubscribersByUid(row.uid)
                             .then(subscribers => {
                                 if (subscribers.length) {
                                     for (const sub of subscribers) {
-                                        const minutesDiff = moment()
-                                            .diff(
-                                                moment(row.ts * 1000).utcOffset(sub.tz),
-                                                'minute'
-                                            )
-                                        ;
                                         BotManager.bot.sendMessage(sub.uid, i18n.t('sub.not.answering', {
                                                 lng: sub.lang,
                                                 name: sub.alias,
@@ -37,14 +42,37 @@ export const NotAnsweringCronHandler = () => {
                                             })
                                         ;
                                     }
+                                    const answersKey = moment(userTsForMoment).utcOffset(row.tz).format(AYOK_DATE_FORMAT);
+                                    const answers = JSON.parse(row.answers)[answersKey] || {};
+                                    const ayokHour = Object.keys(answers).includes('09') ? '20' : '09';
+                                    BotManager.bot.sendMessage(
+                                        row.uid,
+                                        i18n.t('not.responding.message', {lng: row.lang, minutes: minutesDiff}),
+                                        {
+                                            reply_to_message_id: row.message_id,
+                                            reply_markup: {
+                                                inline_keyboard: [
+                                                    [
+                                                        {
+                                                            text: i18n.t('im.ok', {lng: row.lang}),
+                                                            callback_data: `ayok_${ayokHour}_${row.ts}`,
+                                                        }
+                                                    ]
+                                                ],
+                                            }
+                                        }
+                                    );
+                                    db.setWaitQueueProcessed(row.uid);
+                                    BotManager.bot.editMessageReplyMarkup({inline_keyboard: []}, {
+                                        chat_id: row.uid,
+                                        message_id: row.message_id
+                                    });
                                 }
-                                BotManager.bot.deleteMessage(row.uid, row.message_id);
-                                db.deleteFromWaitAnswer(row.uid);
                             })
                         ;
                     }
                 }
             })
         ;
-    });
+    }).start();
 };
