66a7182c707d6d92bdf8fee9d22e7e2cb594113c
Aleksei <krich.al.vl@gmail.com>
2022-03-28 16:54:34 +0300

    fix: set report hours to 21:00

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const LANG_UA = 'lang_ua';
 export const LANG_EN = 'lang_en';
-export const REPORT_HOURS = '22';
+export const REPORT_HOURS = '21';
 export const AYOK_DATE_FORMAT = 'YYYYMMDD';
 export const RESTRICTED_COUNTRIES = ['RU'];
